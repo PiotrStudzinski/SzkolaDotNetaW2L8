cbd1ff2451d699b940c2d69ddff30e605c6bc683
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 20:45:55 +0200

    Zakończenie zadania 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,7 +6,22 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            Console.Write("Give the number of elements in the Fibonacci sequence: ");
+            byte elements = byte.Parse(Console.ReadLine());
 
+            ulong first = 0;
+            ulong second = 1;
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+
+            for (byte i = 1; i < elements - 1; i++)
+            {
+                ulong current = first + second;
+                Console.WriteLine(current);
+
+                first = second;
+                second = current;
+            }
         }
     }
 }
