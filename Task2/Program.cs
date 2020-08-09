0325cb33b8e6d49baf3deb1c9fa7bc2ea4803382
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 20:04:13 +0200

    Zakończenie zadania 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,7 +7,15 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-
+            int count = 1;
+            do
+            {
+                if (count % 2 == 0)
+                {
+                    Console.WriteLine(count);
+                }
+                count++;
+            } while (count <= 1000);
         }
     }
 }
