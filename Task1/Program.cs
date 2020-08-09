fa710d458ab4a6dc83eb057c288f6f32ed6cd818
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-09 19:36:46 +0200

    Zakończenie zadania 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,29 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+            int count = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i != 1)
+                {
+                    int x = 0;
+                    bool isPrime = true;
+                    x = i / 2;
+                    for (int j = 2; j <= x; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"w przedziale od 1 do 100 jest {count} liczb pierwszych.");
         }
     }
 }
