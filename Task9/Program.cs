bb553295b8533eadfcd7ac38d1d6d8e7b464b3ad
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-11 21:46:17 +0200

    Zakończenie zadania 9

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -6,7 +6,30 @@ namespace Task9
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Give the number: ");
+            uint number = uint.Parse(Console.ReadLine());
+
+            string binary = "";
+
+            while (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    binary += "0";
+                }
+                else
+                {
+                    binary += "1";
+                }
+
+                number /= 2;
+            }
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                Console.Write(binary[i]);
+            }
         }
+
     }
 }
