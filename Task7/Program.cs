0534169fb4ba20a9b69ae4ba11c52eb8ceed3e31
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-11 20:43:39 +0200

    Zakończenie zadania 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,7 +6,43 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Give the number: ");
+            uint number = uint.Parse(Console.ReadLine());
+
+            for (uint i = 1; i < number + 1; i++)
+            {
+                for (uint j = 1; j <= (number * 2) - 1; j++)
+                {
+                    if ((j <= number - i) || (j >= number + i))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            for (uint i = number - 1; i > 0; i--)
+            {
+                for (uint j = 1; j <= (number * 2); j++)
+                {
+                    if ((j <= number - i) || (j >= number + i))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
