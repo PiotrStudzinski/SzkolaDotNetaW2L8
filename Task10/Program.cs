f79addfbced3a74c0e1710eda9cbb82987217cbe
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-12 21:40:32 +0200

    Zakończenie zadania 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Task10
 {
@@ -6,7 +7,29 @@ namespace Task10
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Give the number: ");
+            uint numberOne = uint.Parse(Console.ReadLine());
+
+            Console.Write("Give the number: ");
+            uint numberTwo = uint.Parse(Console.ReadLine());
+
+            uint leastMultiple;
+            uint first = numberOne;
+            uint second = numberTwo;
+
+            while (numberOne != numberTwo)
+            {
+                if (numberOne > numberTwo)
+                {
+                    numberOne -= numberTwo;
+                }
+                else
+                {
+                    numberTwo -= numberOne;
+                }
+            }
+            leastMultiple = (first * second) / numberOne;
+            Console.WriteLine($"Least Common Multiple is: {leastMultiple}");
         }
     }
 }
